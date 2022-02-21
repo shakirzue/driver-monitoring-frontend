2c9d4308013ccee72e6ae5b39505055cbb049e22
shakirzue <99166577+shakirzue@users.noreply.github.com>
2022-02-21 13:52:40 +0500

    apply styling to form

--- src/components/action-note.js
+++ src/components/action-note.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import Cookies from 'universal-cookie';
-
+import '../css/main.css';
 const ActionNote = (props) => {
     const [action_id, setActionId] = useState();
     const [note, setNote] = useState();
 
-    const handleChange = (event) => {      
+    const handleChange = (event) => {
         setActionId(props.actionid);
         setNote(event.target.value);
     };
@@ -13,28 +13,31 @@ const ActionNote = (props) => {
     const handleSubmit = (event) => {
         event.preventDefault();
         const cookies = new Cookies();
-        
+
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 'action_id': action_id, 'note': note, email: cookies.get('email'), token: cookies.get('auth') })
         };
-        fetch(process.env.REACT_APP_SERVER_API_URL+'CreateActionNotes', requestOptions)
-        .then(function (response) {
-            response.json();
-        }).then((result)=>{
-            console.log(result);
-            window.location.href = "/view-actions";
-        });
+        fetch(process.env.REACT_APP_SERVER_API_URL + 'CreateActionNotes', requestOptions)
+            .then(function (response) {
+                response.json();
+            }).then((result) => {
+                console.log(result);
+                window.location.href = "/view-actions";
+            });
         event.preventDefault();
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="hidden" name="action_id" value='0'></input>
-            <input type="text" name="note" onChange={handleChange}></input>
-            <input type="submit" value="Submit" />
-        </form>
+        <div className="form-box">
+            <h5 className="form-step"> Create action to assesst driver activities</h5>
+            <form onSubmit={handleSubmit}>
+                <input type="hidden" name="action_id" value='0'></input>
+                <input type="text" name="note" onChange={handleChange}></input>
+                <input type="submit" value="Submit" className="submitBtn"/>
+            </form>
+        </div>
     );
 };
 
